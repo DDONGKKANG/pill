99b18c91c5da46da04d8dd54a31aefa4bafedf5b
yoooun45 <yoooun45@naver.com>
2019-08-20 01:00:39 +0900

    step2와 step5 합침 main 함수 한 폴더를 모두 읽어 step3,step5 폴더에 결과를 저장하도록 함

--- pill_preprocessiong.py
+++ pill_preprocessiong.py
@@ -25,18 +25,22 @@ def step1_getpillarea(image) :
     (success, saliencyMap) = saliency.computeSaliency(resizeImage)
     saliencyMap = (saliencyMap * 255).astype("uint8")  # 0-1사이의 값을 0-255의 값으로 변환
 
+    #cv2.imshow("saliency Map",cv2.resize(saliencyMap, (500, 700), interpolation=cv2.INTER_CUBIC))
     # if we would like a *binary* map that we could process for contours,
     # compute convex hull's, extract bounding boxes, etc., we can
     # additionally threshold the saliency map
     threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255,
                               cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
+
     # 이진화 이미지의 윤곽선을 검색
     # cv2.findContours(이진화 이미지, 검색 방법, 근사화 방법) return 윤곽선, 계층 구조
-    contours, hierachy = cv2.findContours(threshMap, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    contours, hierachy = cv2.findContours(threshMap, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    #cv2.drawContours(resizeImage, contours, 0, (0, 0, 255), 2)
     area, output = 0, contours[0]
     #가장 큰 윤곽선 색
     for cnt in contours:
+        cv2.drawContours(threshMap, cnt, 0, (0, 0, 255), 2)
         if (area < cv2.contourArea(cnt)):
             area = cv2.contourArea(cnt)
             output = cnt
@@ -45,19 +49,19 @@ def step1_getpillarea(image) :
     epsilon = 0.02 * cv2.arcLength(output, True)
     approx = cv2.approxPolyDP(output, epsilon, True)
     #cv2.drawContours(resizeImage, [approx], 0, (0, 0, 255), 2)
-
+    #cv2.imshow("re",cv2.resize(resizeImage, (500, 700), interpolation=cv2.INTER_CUBIC))
     #찾은 물약의 근사치 좌표 구하기
     x, y, w, h = cv2.boundingRect(approx)
     rx = x
     ry = y
-    if (x > 20):
-        rx = x - 20
-    if (y > 20):
-        ry = y - 20
-    #cv2.rectangle(resizeImage, (rx, ry), (x + w + 30, y + h + 30), (0, 255, 0), 2)
+    if (x > 30):
+        rx = x - 30
+    if (y > 100):
+        ry = y - 100
+    #cv2.rectangle(resizeImage, (rx, ry), (x + w + 100, y + h + 100), (0, 255, 0), 2)
 
     #사진 자르기
-    dst = resizeImage[ry:y + h + 30,rx:x + w + 30]  # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]]
+    dst = resizeImage[ry:y + h + 100,rx:x + w + 100]  # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]]
 
     return dst
 
@@ -73,6 +77,9 @@ def step2_del_shadow(img):
     notB3 = np.zeros((height, width))
     B5 = np.zeros((height, width))
     tmp_v = np.zeros((height,width))
+
+    sum1 = 0
+    sum2 = 0
 
     for i in range(height):
         for j in range(width):
@@ -91,16 +98,12 @@ def step2_del_shadow(img):
             else:
                 B5[i][j] = 0
 
-    sum1 = 0
-    sum2 = 0
+            sum1 += notB3[i][j] * tmp_v[i][j]
+            sum2 += notB3[i][j]
+
 
     # Calculate m3 _ modify
-    for x in range(height):
-        for y in range(width):
-            sum1 += notB3[x][y] * tmp_v[x][y]
-            sum2 += notB3[x][y]
     m3 = sum1 / sum2
-
     # Calculate Bw
     Bw = B3 + 0.7*B5
 
@@ -119,25 +122,6 @@ def step2_del_shadow(img):
     result = cv2.cvtColor(result_hsv, cv2.COLOR_HSV2BGR)
 
     return result
-
-def step2_1_do_contour(img):
-    #blurred = cv.GaussianBlur(img, (5,5), 0)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 127, 255, 0)
-
-    thresh = cv2.erode(thresh, None, iterations=2)
-
-    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    if len(contours) == 2:
-        contours = contours[0]
-    elif len(contours) == 3:
-        contours = contours[1]
-
-    cv2.drawContours(img, contours, -1, (0,0,255), 1)
-
-    return img
 
 def step3_extractpill(img):
     # gray -> adaptive Threshold
@@ -215,15 +199,27 @@ def step4_matchshape(img):
 
     print("min" + str(min))
 
-def step5_1_intensity(img):
+def step5_1_grayscale(img):
     """
-    step5_1-밝기 조절
+    step5_1-이미지 회색처리
+    새로운 폴더에 저장했다가 불러올 때 회색으로 불러옴.
+    :param img:
+    :return:
+    """
+    path = os.getcwd()
+    cv2.imwrite(os.path.join(path, 'step4.jpg'), img)
+    img = cv2.imread(os.path.join(path, 'step4.jpg'), cv2.IMREAD_GRAYSCALE)
+    return img
+
+def step5_2_intensity(img):
+    """
+    step5_2-밝기 조절
     :param img:
     :return:
     """
     for i in range(0, img.shape[0]):
         for j in range(0, img.shape[1]):
-            print(i,j,img[i,j])
+            #print(i,j,img[i,j])
             if img[i, j] > 225:
                 img[i, j] = 235
                 continue
@@ -243,15 +239,12 @@ def step5_1_intensity(img):
                 img[i, j] = 0
                 continue
             img[i, j] += 50
-
     return img
 
-def step5_2_magnitude (img, a, b):
+def step5_3_magnitude (img):
     """
-
+    step5_3-Gaussian Filter
     :param img:
-    :param a:
-    :param b:
     :return:
     """
     img = cv2.GaussianBlur(img, (15, 15), 0)
@@ -259,15 +252,26 @@ def step5_2_magnitude (img, a, b):
     sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
     dxabs = cv2.convertScaleAbs(sobelx)
     dyabs = cv2.convertScaleAbs(sobely)
-    img = cv2.addWeighted(dxabs, a, dyabs, b, 0)
+    img = cv2.addWeighted(dxabs, 0.7, dyabs, 0.7, 0)
     return img
 
-def step5_3_otsu(img):
+def step5_4_otsu(img):
+    """
+    step5_4-블러처리 후 이진화
+    :param img:
+    :return:
+    """
     blur = cv2.GaussianBlur(img, (11, 11), 0)
     ret, img = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return img
 
-def step5_4_imshow_components(img):
+def step5_5_imshow_components(img):
+    """
+    step5_5-테스트용 라벨링
+    step5_5는 사실 확인용임 실제 이미지에 적용되면 안됨.
+    :param img:
+    :return:
+    """
     # image connected component
     ret, labels = cv2.connectedComponents(img)
 
@@ -284,7 +288,14 @@ def step5_4_imshow_components(img):
 
     return labeled_img
 
-def step5_5_contours(img,src, what):
+def step5_6_contours(img,src):
+    """
+    step5_6-Bounding Box추출
+    적절한 contours를 찾아서 Bounding Box 그리기
+    :param img:
+    :param src:
+    :return:
+    """
     c, h = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     a1 = 0
     a2 = 0
@@ -338,75 +349,73 @@ def step5_5_contours(img,src, what):
         else:
             continue
 
-        if what == 1:
-            rect = cv2.minAreaRect(cnt)
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
-            result_img = cv2.drawContours(src, [box], 0, (0, 0, 255), 2)
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        result_img = cv2.drawContours(src, [box], 0, (0, 0, 255), 2)
 
-        if what == 0:
-            # Minimum Enclosing Circle
-            (cx, cy), radius = cv2.minEnclosingCircle(cnt)
-            center = (int(cx), int(cy))
-            radius = int(radius)
-            result_img = cv2.circle(src, center, radius, (255, 255, 0), 3)  # yellow
+    return result_img, a5
 
-    print (len(c))
-    print (a1)
-    print (a2)
-    print (a3)
-    print (a4)
-    print (a5)
-
-    return result_img
 
 if __name__ == "__main__":
-    #파일 읽어오기
-    cur_path = os.getcwd()
 
-    #폴더 읽어오기
-    path= os.path.join(cur_path,"pill_image")
+    #파일 읽어오기(이미지 1개)
+    # image = cv2.imread("gg1.jpg")
+    # cv2.imshow("original", image)
+
+    #폴더로 읽어오기
+    cur_path = os.getcwd()
+    print(cur_path)
+
+    path= os.path.join(cur_path,"pill_image") #pill_image = 약 이미지 폴더
     imagepaths_list = [os.path.join(path, file_name) for file_name in os.listdir(path)]
-    print(imagepaths_list)
+    #print(imagepaths_list)
+    a = 1
     for i in imagepaths_list :
+        print(a)
         image = cv2.imread(i)
 
-    print(cur_path)
-    #image = cv2.imread("test2.png")
-    #cv2.imshow("original", image)
+        # step1 실행
+        print("step1 start")
+        step1 = step1_getpillarea(image)
+        # cv2.imshow("step1", step1)
 
-    #step1 실행
-    print("step1 start")
-    step1 = step1_getpillarea(image)
-    cv2.imshow("step1", step1)
+        # step2 실행
+        print("step2 start")
+        step2 = step2_del_shadow(step1)
+        # cv2.imshow("step2", step2)
 
-    #step2 실행
-    print("step2 start")
-    step2 = step2_del_shadow(step1)
-    cv2.imshow("step2", step2)
-    #step2_1 = contour_result = step2_1_do_contour(step2)
-    #cv2.imshow("step2-1", step2_1)
+        # step3 실행
+        print("step3 start")
+        mask, step3 = step3_extractpill(step2)
+        outpath = os.path.join(cur_path,"step3")
+        cv2.imwrite(os.path.join(outpath,str(a)+".jpg"), step3)
+        #cv2.imshow("step3", step3)
 
-    #step3 실행
-    print("step3 start")
-    mask,step3 = step3_extractpill(step2)
-    cv2.imshow("step3", step3)
+        # step4 실행
+        print("step4 start")
+        step4 = step3
 
-    #step4 실행
-    print("step4 start")
-    #step4_matchshape(mask)
+        # step5 실행
+        print("step5 start")
+        tmp = step4
+        tmp = step5_1_grayscale(tmp)
+        # tmp = step5_2_intensity(tmp)
+        tmp = step5_3_magnitude(tmp)
+        tmp = step5_4_otsu(tmp)
+        check_label = step5_5_imshow_components(tmp)
+        # cv2.imshow("check_label", check_label)
+        step5, check = step5_6_contours(tmp, step4)
+        if check == 2:
+            print("done!")
+            outpath = os.path.join(cur_path, "step5")
+            cv2.imwrite(os.path.join(outpath, str(a) + ".jpg"), step5)
 
-    #step5 실행
-    print("step5 start")
-    """
-    현재 오류
-    step4 = step3
-    tmp = step4
-    tmp = step5_1_intensity(step4)
-    tmp = step5_2_magnitude(tmp,0.7,0.7)
-    tmp = step5_3_otsu(tmp)
-    tmp = step5_4_imshow_components(tmp)
-    step5 = step5_5_contours(tmp,step4,1)
-    cv2.imshow("step5",step5)
-    """
+        a = a + 1
+
     cv2.waitKey(0)
+
+
+
+
+
